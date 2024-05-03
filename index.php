75fb4bc2f4e0eec1e7628e6a30775a70d9833ec3
ManashModi <86552684+Manashmodi@users.noreply.github.com>
2024-05-03 11:48:43 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Olympic VR Experience</title>
+  <script src="https://aframe.io/releases/1.2.0/aframe.min.js"></script>
+</head>
+<body>
+  <a-scene>
+    <!-- Video -->
+    <a-video src="iii.mp4" width="16" height="9" position="0 2 -9"></a-video>
+
+    <!-- Sky background -->
+    <a-sky src="m.jpg" rotation="0 -90"></a-sky>
+  </a-scene>
+</body>
+</html>
